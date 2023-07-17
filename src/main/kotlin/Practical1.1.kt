b3eac7e8c10bf7f0853ff1364444916e85793811
student <student@gnu.ac.in>
2023-07-17 10:40:49 +0530

    Commit

--- src/main/kotlin/Practical1.1.kt
+++ src/main/kotlin/Practical1.1.kt
@@ -1,4 +1,21 @@
 fun main()
 {
-    println("Hello World")
+    var a:Int=82
+    var b:Float=34.0F
+    var c:Double=4.5
+    var d:String="hiii"
+    var e:Long=1227
+    var f:Short=4
+    var g:Char='T'
+
+
+
+    println("Integer:$a")
+    println("Float:$b")
+    println("Double:$c")
+    println("String:$d")
+    println("$e")
+    println("$f")
+    println("$g")
+
 }
