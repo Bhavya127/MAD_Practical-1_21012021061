3ece6db6d2a6b5de0fb4e330638becc5caef0bbc
student <student@gnu.ac.in>
2023-07-18 09:35:23 +0530

    practical1.2

---
+++ src/main/kotlin/Practical1.2.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    var a:Int=82
+    println("Integer Value:$a")
+    var b:Double=a.toDouble()
+    println("Double Value:$b")
+
+    var c:String="102"
+    println("String:$c")
+    var d:Int=c.toInt()
+    println("Integer Value:$d")
+
+    var e:Double=c.toDouble()
+    println("Double value:$e")
+
+}
